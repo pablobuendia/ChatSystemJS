005a4b9cdf941f40f4ff27a386aff5f413ca5aaf
ConstanzaPellegrini <constanza.p.03@gmail.com>
2020-11-13 22:14:29 -0300

    Modificacion del broker y del cliente

--- Broker/broker.js
+++ Broker/broker.js
@@ -2,8 +2,10 @@
 const zmq = require('zeromq');
 const subSocket = zmq.socket('xsub');
 const pubSocket = zmq.socket('xpub');
-var port;
+
 var direccion = 'tcp://127.0.0.1:';
+
+var listaTopicos = [];
 
 const readline = require('readline');
 var rl = readline.createInterface({
@@ -29,19 +31,18 @@ O bien podemos pedirle al usuario que ingrese los dos puerto manualmente o pedir
 Ej: 3000 (se ingresa) y el siguiente usado es 3001
 La desventaja es que si otra persona que no sea nosotros lo corre no va a saber que el 3001 está en uso y puede llevar
 a un error.
+
+Elegimos que el puerto de pubSocket se ingrese solo porque no pudimos hacer que se ingrese manualmente ambos. 
 */
-
-
-
 
 // redirige todos los mensajes que recibimos
 subSocket.on('message', function (topic, message) {
-  pubSocket.send([topic, message])
+    console.log('LLego un mensaje para publicar en:', topic);
+    pubSocket.send([topic, message]);
 });
 
 // cuando el pubSocket recibe un tópico, subSocket debe subscribirse a él; para eso se utiliza el método send
+//El broker que maneja "message/all" no tiene que mandar el mensaje al cliente que lo publico
 pubSocket.on('message', function (topic) {
 	subSocket.send(topic)
 });
-
-//El broker que maneja "message/all" no tiene que mandar el mensaje al cliente que lo publico
--- Cliente/cliente.js
+++ Cliente/cliente.js
@@ -1,31 +1,50 @@
 const readline = require('readline');
+var id_cliente; //----------------- IMPORTANTE 
 var r1 = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+r1.question('Ingrese su id: ', (answer) => {
+    id_cliente = answer;
+}); 
+
 // subber.js
 const zmq = require('zeromq'),
-			subSocket = zmq.socket('sub');
+            subSocket = zmq.socket('sub'),
+            pubSocket = zmq.socket('pub');
+/*
+La conexión siguiente se tiene que hacer a partir de la devolución del coordinador a donde se tiene que conectar
+*/
+subSocket.connect('tcp://127.0.0.1:3001');
+pubSocket.connect('tcp://127.0.0.1:3000');
+subSocket.subscribe('All');
 
-subSocket.connect('tcp://127.0.0.1:3001')
-subSocket.subscribe('miTopico1');
-
+/* 
+PREGUNTA: por cada broker al que se quiere conectar debe tener un subSocket y un pubSocket? ---------------------------------- PREGUNTA
+Porque se tiene que conectar a diferentes puertos para recibir mensaje de los distintos topicos
+*/
 subSocket.on('message', function(topic, message) {
-  console.log('Recibio topico:', topic.toString(), 'con mensaje:', message.toString())
+    let mensaje = message.toString();
+    mensaje = JSON.parse(mensaje);
+    console.log('Recibio topico: ', topic.toString(), ' con mensaje: ', mensaje.mensaje);
 });
 
-// pubber.js
-pubSocket = zmq.socket('pub');
 
-pubSocket.connect('tcp://127.0.0.1:3000');
-
+r1.on('line', (mensaje) => {
+    let arrayMensaje = mensaje.split(':');
+    let fecha = new Date();
+    fecha = fecha.toISOString();
+    let message = '{"emisor":"'+id_cliente+'", "mensaje":"'+arrayMensaje[1]+'", "fecha":"'+fecha+'"}';
+    pubSocket.send([arrayMensaje[0], message]);
+    r1.close();
+}); //MEJORAR, solamente permite que envie 1 mensaje y hasta ahí llego. 
 //Tener en cuenta que el cliente siempre esta esperando que le ingresen un mensaje para publicar si es que es publisher. 
- /*
-let counter = 0;
+/*
+Se espera que el mensaje ingresado para ser enviado contenga el topico 
+Ejemplo:
+    All: hola
+    id_cliente: hola
+*/
 
-setInterval(function() {
-	console.log('Envia mensaje nº', counter)
-    pubSocket.send(['miTopico1', 'Este es el mensaje ' + counter++])
-}, 500);*/
 
