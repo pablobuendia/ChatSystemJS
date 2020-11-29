2e13994d9e0f4e03f20f3ffb67e99cbbe8e7e5a1
Ezequiel <ezequiel.fritzios@gmail.com>
2020-11-29 17:08:26 -0300

    Add delete endpoint in order to be allowed to delete topic

--- Cliente/service.js
+++ Cliente/service.js
@@ -23,10 +23,29 @@ function getTopicsList() {
             displayTopicsList(topicsArray);
         } 
         else {
-            displayTopicsList(["Error"]);
-            console.log("error");
+            alert("Error al obtener la lista de topicos");
         }  
     }
     xmlHttp.open("GET", topicsListUrl, true); // true for asynchronous 
     xmlHttp.send();
+}
+
+function deleteTopic() {
+    let topicToDelete = document.getElementById("borrarTopico").value;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (this.readyState == 4 && xmlHttp.status == OK) {
+            alert("Topico borrado correctamente");
+        } 
+        else {
+            alert("Error al borrar el topico "+ topicToDelete);
+        } 
+    }
+    // DEBERIAMOS MANDAR EL TOPICTODELETE COMO PARAMETRO DE LA REQUEST
+    const json = {
+        "topic": topicToDelete
+    };
+    xmlHttp.open("DELETE", topicsListUrl, true);
+    xmlHttp.setRequestHeader('Content-Type', 'application/json');
+    xmlHttp.send(JSON.stringify(json));
 }
