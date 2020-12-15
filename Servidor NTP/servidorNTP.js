d8b3b4e7a4e1454a43c51143696afc0abe05e012
Pablo Buendia <buenaspablo@gmail.com>
2020-12-15 13:30:20 -0300

    corregir ntp

--- Servidor NTP/servidorNTP.js
+++ Servidor NTP/servidorNTP.js
@@ -15,8 +15,13 @@ var server = net.createServer(function (socket) {
     socket.write(JSON.stringify({ t1: JSON.parse(data).t1, t2 : T2, t3: T3}));
   });
 
+  socket.on('error', (err) => {
+    console.log(err);
+  })
+
 });
 
 server.listen(port, () => {
   console.log("Servidor NTP escuchando en puerto:" , port);
 });
+
